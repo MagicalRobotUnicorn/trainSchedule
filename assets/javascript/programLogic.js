c58d46d557e8cc04ba3203ec5e91ccd611567e0e
John Henry Weber <johnhenryweber@gmail.com>
2019-09-15 00:06:19 -0700

    Display Actually Works

--- assets/javascript/programLogic.js
+++ assets/javascript/programLogic.js
@@ -42,14 +42,13 @@ const $trainList = $('#trainList');
 
 // db.ref().push(trains);
 
-db.collection('trains').get().then((snapshot) =>{
+db.collection('trains').get().then((snapshot) => {
     snapshot.docs.forEach((doc) => {
-        console.log(doc.data());
+
         var $newRow = $('<tr>');
         var $trainName = $('<td>').append(doc.data().name);
         var $destination = $('<td>').append(doc.data().destination);
         var $frequency = $('<td>').append(doc.data().frequency);
-        var $firstTrain = $('<td>').append(doc.data().firstTrain);
 
         // Moment time stamp now
         var currentTime = moment().format("HHmm");
@@ -60,30 +59,50 @@ db.collection('trains').get().then((snapshot) =>{
         var nextTrain = firstTrain;
 
         // Function that adds interval until is it larger than now
-        while(nextTrain.format("HHmm") < currentTime) {
+        while (nextTrain.format("HHmm") < currentTime) {
             nextTrain.add(doc.data().frequency, 'm');
+
+            if (nextTrain.format("HH") == 00) {
+                nextTrain = "----";
+                break;
+            }
         }
 
         // Function that extrapolates how much time is left until the next train
-        var duration = moment.duration(nextTrain.diff(moment()));
-        console.log(duration);
-        var minUntil = duration.asMinutes();
-        console.log(minUntil);
-        
-        var displayNextTrain = nextTrain.format("HHmm");
+        if (nextTrain !== "----") {
+            var duration = moment.duration(nextTrain.diff(moment()));
+            var minUntil = (duration.asMinutes());
+        } else {
+            var duration = moment.duration(firstTrain.diff(moment()));
+            var minUntil = (duration.asMinutes());
+        }
+
+        // Display only military time of next train
+        if (nextTrain !== "----") {
+            var displayNextTrain = nextTrain.format("HHmm");
+        } else {
+            var displayNextTrain = "----";
+        }
+
+        // Create jQuery object cells of remaining fields
+        var $nextTrain = $('<td>').append(displayNextTrain);
+        var $minUntil = $('<td>').append(parseInt(minUntil));
 
 
+
+        // Individual cells
         $newRow.append($trainName);
         $newRow.append($destination);
         $newRow.append($frequency);
-        $newRow.append(displayNextTrain);
-        $newRow.append(minUntil);
+        $newRow.append($nextTrain);
+        $newRow.append($minUntil);
 
+        // Append row
         $trainList.append($newRow);
 
     })
 });
 // db.ref().on("child_added", function(snapshot){
-    
+
 
 // });
